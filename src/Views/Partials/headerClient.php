ce11fda8f7e0c76b514122e0369f31b5be0d39c7
Daniel Coto Quiros <danielcotoquiros@hotmail.com>
2019-11-20 04:53:11 -0600

    Commit diseno login

--- src/Views/Partials/headerClient.php
+++ src/Views/Partials/headerClient.php
@@ -7,7 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="http://cycwebservice.cf/style.css">
     <link rel="stylesheet" href="http://cycwebservice.cf/size.css">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST']; ?>/Assets/CSS/general.css">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST']; ?>/Assets/CSS/fidestore.css">
     <title>FideStore</title>
 </head>
 
 <body>
+    <nav class="nav text-white position-sticky">
+        <a href="http://<?= $_SERVER['HTTP_HOST']; ?>" class="nav-title">FideStore</a>
+        <ul class="nav-list">
+            <li class="nav-item"><a href="#" class="text-white">Productos</a></li>
+            <li class="nav-item"><a href="#" class="text-white">Acerca</a></li>
+            <li class="nav-item nav-item-cart"><a href="#" class="text-white">Carrito</a></li>
+            <li class="nav-item"><a href="#" class="text-white">Mi cuenta</a></li>
+        </ul>
+        <div class="btn-action">
+        </div>
+    </nav>
