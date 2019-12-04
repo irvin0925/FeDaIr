7b593b55846181280bfce79c71c8ace7d1735b0a
Daniel Coto <49823292+danielCQgt4@users.noreply.github.com>
2019-12-03 19:53:05 -0600

    Agregando productos
    He agregado un bagel y una computadora, mas las categiorias

--- db/script.sql
+++ db/script.sql
@@ -1,1 +1,10 @@
--- Scripts for FactuCyC
+-- Scripts for FideStore
+-- Categorias
+insert into CategoriaProducto (descripcion) values 
+('Tecnologico'),('Hogar'),('Comida'),('Electronico'),('Comunicacion');
+
+-- Productos
+insert into Producto (nombre,descripcion,precio,cantidadDisponible,cantCompras,urlImg,idCategoriaProducto) 
+value ('Computadora','Computadora todo en 1, monitor y componentes',450000,5,0,'temp_pc.png',1);
+insert into Producto (nombre,descripcion,precio,cantidadDisponible,cantCompras,urlImg,idCategoriaProducto) 
+value ('Bagels','Disfruta de los exquisitos bagels, crujientes',5500,5,0,'temp_bagel.png',3);
